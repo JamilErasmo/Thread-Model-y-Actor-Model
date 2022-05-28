5603291d49d22dfd0c7aff9a09119b6b482142a3
jamil quituizaca <JamilErasmo>
2022-05-27 23:49:13 -0500

    grupal

---
+++ TrabajoGrupal/src/main/java/ActorModel/Actor1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ActorModel;
+
+import akka.actor.AbstractActor;
+/**
+ *
+ * @author Usuario
+ */
+public class Actor1 extends AbstractActor{
+    
+  
+}
